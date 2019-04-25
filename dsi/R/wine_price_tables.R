0c2f9cc94bcfccf2859e6dbded5fb2f501312e5b
Jane Carlen <janecarlen@administorsmbp3.lan>
2019-04-25 13:28:12 -0400

    removed unnecessary pasted code

--- dsi/R/wine_price_tables.R
+++ dsi/R/wine_price_tables.R
@@ -36,17 +36,6 @@ library(cluster)
 library(changepoint)
 library(RecordLinkage)
 
-function (pix, binaryThreshold = 150, background = 255, angle = NA) 
-{
-  pix = as(pix, "Pix")
-  pix = pixConvertTo8(pix)
-  if (is.na(angle)) {
-    bin = pixThresholdToBinary(pix, binaryThreshold)
-    angle = pixFindSkew(bin)
-  }
-  pixRotateAMGray(pix, angle[1] * pi/180, background)
-}
-
 wd = "~/Documents/DSI" #path to wine-price-extraction repo
 setwd(wd)
 
