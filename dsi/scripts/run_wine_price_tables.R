f51a61727fc0c229d8ee1c31834ec739f2b74ad0
Jane Carlen <janecarlen@campus-043-213.ucdavis.edu>
2019-05-03 16:16:37 -0700

    more small modificatons to run from command line eventually

--- dsi/scripts/run_wine_price_tables.R
+++ dsi/scripts/run_wine_price_tables.R
@@ -1,5 +1,6 @@
 # See R/wine_price_tables.R for overview of the workflow and To Do's associated with price_table_extraction function called below
 # Jane Carlen
+# Notes, assumes .jpg image files
 
 ####################################################################################################
 # Setup ####
@@ -21,9 +22,10 @@ source("wine-price-extraction/dsi/R/wine_price_nameBoxes.R") #redundant
 source("wine-price-extraction/dsi/R/helper.R") #redundant
 source("wine-price-extraction/dsi/R/wine_price_tables.R")
 
+FILESET = "/Users/janecarlen/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages"
 OUTPUT.FOLDER = "/Users/janecarlen/Documents/DSI/wine-price-extraction/dsi/Data"
-DATA.INPUT.FOLDER = "~/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages/fullboxes_deskewed"
-DATA.OUTPUT.FOLDER = "~/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages/fullboxes_deskewed"
+DATA.INPUT.FOLDER = "/Users/janecarlen/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages/fullboxes_deskewed"
+DATA.OUTPUT.FOLDER = "/Users/janecarlen/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages/fullboxes_deskewed"
 SAVE.DATA = FALSE
 
 args = commandArgs(trailingOnly = TRUE)
@@ -78,12 +80,23 @@ if (!file.exists (DATA.OUTPUT.FOLDER) ) {
 # 1835 (before fixing, too few words in name), 0008, 1470
 # 0550 two types of IDs on one page
 
+# Other possible filesets:
+
+# pages with lots of marks
+# marks_top_files = read.csv("wine-price-extraction/dsi/Data/marks_top_files.csv", stringsAsFactors = F)
+# 0939.jpg needs higher threshold, eg  200
+
+# pages we have truth for
+# truth_files = unique(unlist(sapply(list.files("~/Downloads/price_id_truth_RDS_files", recursive = T), str_extract, pattern =   "UCD_Lehmann_[0-9]{4}"))) #downloaded then deleted
+# truth_files = truth_files[-grep(truth_files, pattern = "2759")] #remove bad one
+#-- files in truth not in sample folder: fileset = c("UCD_Lehmann_0008", "UCD_Lehmann_0027", "UCD_Lehmann_0267", "UCD_Lehmann_1470  ", "UCD_Lehmann_1994", "UCD_Lehmann_1544", "UCD_Lehmann_1835")
+
 # 1. Example ----
 
 if (RUN.FILE) {
-  
-file1 = FILESET
 
+file1 = FILESET
+  
 ### will remove later ###
 file1 = "UCD_Lehmann_0011"# 1106
 
@@ -117,62 +130,52 @@ price_table_extraction(file1,
 # 2. Run on a fileset ----
 
 if (!RUN.FILE) {
-  
-  fileset = str_extract(list.files("~/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages"), ".*[^\\.jpg]")
-  # files in truth not in sample folder: fileset = c("UCD_Lehmann_0008", "UCD_Lehmann_0027", "UCD_Lehmann_0267", "UCD_Lehmann_1470  ", "UCD_Lehmann_1994", "UCD_Lehmann_1544", "UCD_Lehmann_1835")
-  
-  # Other possible filesets:
-  
-  # pages with lots of marks
-  # marks_top_files = read.csv("wine-price-extraction/dsi/Data/marks_top_files.csv", stringsAsFactors = F)
-  # 0939.jpg needs higher threshold, eg  200
-  
-  # pages we have truth for
-  # truth_files = unique(unlist(sapply(list.files("~/Downloads/price_id_truth_RDS_files", recursive = T), str_extract, pattern =   "UCD_Lehmann_[0-9]{4}"))) #downloaded then deleted
-  # truth_files = truth_files[-grep(truth_files, pattern = "2759")] #remove bad one
 
-  fileset = subset(fileset, str_detect(fileset, "UCD_Lehmann_[0-9]{4}"))
-  output = vector("list", length(fileset))
-  names(output) = fileset
-
-  i = 0; pix.threshold = NULL; pix.newValue = NULL
+  fileset = FILESET
+  fileset = list.files(FILESET, full.names = T, pattern = "\\.") #only files, not folders
+  PIX.THRESHOLD = NULL; PIX.NEWVALUE = NULL
   
-  if (!file.exists(img1)) {
-    if (!exists("sample_files")) {sample_files = readRDS("~/Documents/DSI/wine-price-extraction/dsi/Data/sample_files.RDS")}
-    folder = sample_files[which(sample_files$file == file1),"Sample"]
-    img1 = paste("~/Documents/DSI/OCR_SherryLehmann/Sample/", folder, "/", file1, ".jpg", sep = "")
-  }
-  
-  for (file1 in fileset) {
-  i = i+1; cat(file1, i, "\n")
-  img1 = paste("~/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages/", file1, ".jpg", sep = "")
-  if (!file.exists(img1)) {
-    if (!exists("sample_files")) {
-      sample_files = readRDS("~/Documents/DSI/wine-price-extraction/dsi/Data/sample_files.RDS")
+  output = lapply(fileset, function(img1) {
+    
+    height1 = dim(readJPEG(img1))[1] #note we'll use the image attribute here later
+    px1 = deskew(pixConvertTo8(pixRead(img1)))
+    api1 = tesseract(px1)
+    if( GetSourceYResolution(api1)==0 ) {SetSourceResolution(api1, 600)}
+    
+    # Should we use pre-run get boxes?
+    if (!is.null(DATA.INPUT.FOLDER)) {
+      DATA1 = readRDS(file.path(DATA.INPUT.FOLDER, paste0(nth(strsplit(img1, split = c("/|\\."))[[1]], -2), "_data1.RDS")))
+    } else {
+      # this is done here instead of in price table extraction so that we know whether to continue
+      DATA1 = GetBoxes(px1, pageSegMode = 6, engineMode = 3)
     }
-    folder = sample_files[which(sample_files$file == file1),"Sample"]
-    img1 = paste("~/Documents/DSI/OCR_SherryLehmann/Sample/", folder, "/", file1, ".jpg", sep = "")
-  }
-  height1 = dim(readJPEG(img1))[1] #note we'll use the image attribute here later
+    
+    # Use getBoxes output in data1 to classify if a page (probably) has readable price tables
+
+    if (median(DATA1$confidence) > 30 & sum(isPrice(DATA1$text, dollar = FALSE)) >= 3) {
+      output1 = try({ # names will tell us if it was successful, but output is stored expernally
+        names( 
+          price_table_extraction(img1, 
+                                      data1 = DATA1,
+                                      output.folder = OUTPUT.FOLDER, 
+                                      data.output.folder = DATA.OUTPUT.FOLDER,
+                                      save.data = SAVE.DATA,
+                                      save.deskewed = FALSE,
+                                      pix.threshold = PIX.THRESHOLD, pix.newValue = PIX.NEWVALUE, #pix.threshold = 200, pix.newValue = 0
+                                      column.header = c("bottle", "case", "quart", "fifth", "half", "of", "24"),
+                                      res1 = 600,
+                                      image.check = FALSE, 
+                                      show.ggplot = FALSE)
+        )
+      })
+    } else {output1 = NULL}
   
-  px1 = deskew(pixConvertTo8(pixRead(img1)))
-  api1 = tesseract(px1)
-  if(GetSourceYResolution(api1)==0) {SetSourceResolution(api1, 600)}
-  #data1 = fullBoxes[[paste0(file1,".jpg")]] # don't use this because not deskewed
-  if(file.exists(paste0("~/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages/fullboxes_deskewed/",file1,"_data1.RDS"))) {
-    data1 = readRDS(paste0("~/Documents/DSI/OCR_SherryLehmann/SampleCatalogPages/fullboxes_deskewed/",file1,"_data1.RDS"))
-  } else {
-    # this seems to return the same results as applying GetBoxes to px1, but not warning message
-    data1 = GetBoxes(api1, pageSegMode = 6, engineMode = 3)}
-  if (median(data1$confidence) > 30 & sum(isPrice(data1$text, dollar = FALSE)) >= 3) {
-    try({output[[i]] = price_table_extraction(file1, image.check = FALSE, data = data1, show.ggplot = FALSE, save.deskewed = TRUE)})
-  }
-  if (is.null(output[[i]])) {
-    output[[i]]= c("median_conf" = median(data1$confidence), "n_price" = sum(isPrice(data1$text, dollar = FALSE)))
-  }
-}
-
-  which(sapply(output, function(x) is.null(names(x[[1]]))))
-  which(sapply(output, function(x) length(names(x[[1]])))!=5)
-
+    if (is.null(output1)) {cat("\nNO PRICE TABLES DETECTED IN", img1, "\n\n")}
+    output1 = c(output1, "median_conf" = median(DATA1$confidence), "n_price" = sum(isPrice(DATA1$text, dollar = FALSE)))
+   
+    return(output1)
+  })
+  
+  names(output) = fileset
+  
 }
