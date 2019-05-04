f4ab1685b4ced3c5613d923c8f1ca28d09d41297
Jane Carlen <janecarlen@administorsMBP3.attlocal.net>
2019-05-04 11:56:55 -0700

    cleaned run_wine_evaluate.R to set up running from command line

--- dsi/scripts/run_wine_evaluate.R
+++ dsi/scripts/run_wine_evaluate.R
@@ -11,10 +11,30 @@ library(stringr)
 library(dplyr)
 library(RecordLinkage)
 
-source("~/Documents/DSI/wine-price-extraction/dsi/R/wine_evaluate.R")
+#source("~/Documents/DSI/wine-price-extraction/dsi/R/wine_evaluate.R")
 
-output.directory = "~/Documents/DSI/wine-price-extraction/dsi/Data/sample_output"
-truth.directory = "~/Documents/DSI/wine-price-extraction/dsi/Data/price_id_truth"
+EVAL.INPUT.DIR = "/Users/janecarlen//Documents/DSI/wine-price-extraction/dsi/Data/price_table_output"
+TRUTH.DIR = "/Users/janecarlen/Documents/DSI/wine-price-extraction/dsi/Data/price_id_truth"
+
+args = commandArgs(trailingOnly = TRUE)
+
+if (length(args) > 0) {
+  EVAL.INPUT.DIR = args[1] 
+  if (length(args) > 1) {
+    TRUTH.DIR = args[2] # Where manually created truth file .RDS live
+    if (length(args) > 2) {
+      EVAL.OUTPUT.DIR = args[3]
+    }
+  }
+}
+
+if (!file.exists (EVAL.INPUT.DIR) ) {
+  stop("Path to input data (price table output RDS files) not valid. Stopping.")
+} 
+
+if (!file.exists (TRUTH.DIR) ) {
+  stop("Path to folder containing truth files (.RDS) not valid. Stopping.")
+} 
 
 # file.number = "0069"
 # test.prices = readRDS(file.path(output.directory, paste0("UCD_Lehmann_", file.number,".RDS")))$prices
@@ -32,11 +52,10 @@ truth.directory = "~/Documents/DSI/wine-price-extraction/dsi/Data/price_id_truth
 # A. Basic stats for all files ####
 
     # internal check ----
-fileset1 = str_extract(list.files(output.directory), pattern = "UCD.*RDS")
-fileset1 = fileset1[!is.na(fileset1)]
+fileset1 = list.files(EVAL.INPUT.DIR, pattern = ".RDS", full.names = TRUE, recursive = FALSE)
+
 evaluate.output = vector("list", length(fileset1)) #returns list
 for (i in 1:length(fileset1)) {
-  file1 = file.path(output.directory,fileset1[i])
   rds1 = readRDS(file1)$prices
   eval1 = wine.evaluate(rds1)
   evaluate.output[[i]] = eval1
@@ -53,133 +72,56 @@ output_summary_internal = data.frame(t(sapply(evaluate.output, function(eval) {
     "n.unsorted" = paste(unlist(sapply(eval$table.ordering, sapply, first)), collapse = ", "), #number of prices than next one in column
     "mean.unsorted" =  paste(unlist(sapply(eval$table.ordering, sapply, nth, 2)), collapse = ", ")#mean of how much they're greater by
   )
-})))
-rownames(output_summary_internal) = fileset1
+  })),  
+  row.names = sapply(sapply(fileset1, str_split, "/"), last))
 
-write.csv(output_summary_internal, "~/Documents/DSI/wine-price-extraction/dsi/Data/output_summary_internal.csv")
+write.csv(output_summary_internal, file.path(EVAL.OUTPUT.DIR, "output_summary_internal.csv"))
 
     # plot that ----
-output_summary_internal_singlestat = ggplot(
-  melt(output_summary_internal %>% dplyr::select(c("n.tables", "n.columns.total", "n.entries.total")), id.vars = NULL) %>%
-                                              mutate(value = as.numeric(value))) + 
+output_summary_internal_singlestat = ggplot( melt(output_summary_internal %>% 
+                                                    dplyr::select(c("n.tables", "n.columns.total", "n.entries.total")), id.vars = NULL) %>%
+                                               mutate(value = as.numeric(value))) + 
   geom_histogram(aes(x = value, group = variable)) + facet_grid(~variable, scales = "free_x")
-ggsave(output_summary_internal_singlestat, filename = "~/Documents/DSI/wine-price-extraction/dsi/Data/output_summary_internal_singlestat.png")
+
+ggsave(output_summary_internal_singlestat, filename = file.path(NAME.OUTPUT.DIR, "output_summary_internal_singlestat.png"))
 
 # B. Compare to truth For files we have truth for ####
 
-truth.directory2 = "~/Documents/DSI/wine-price-extraction/dsi/Data/price_id_truth"
-truth.subdirectory2 = list.dirs(truth.directory2, recursive = F)
+# Assumes we might have the truth files organized by folder (=  name of truth maker)
+truth.subdir = list.dirs(TRUTH.DIR, full.names = T, recursive = T)
 
-summary.output = lapply( truth.subdirectory2, function(elem) {
+summary.output = lapply(truth.subdir, function(elem) {
 
-  fileset.truth = str_extract(list.files(elem), "[0-9]{4}")
+  fileset.truth = list.files(elem, pattern = ".RDS", full.names = TRUE)
   summary.output = vector("list", length = length(fileset.truth))
-  names(summary.output) = paste0("UCD_Lehmann_",fileset.truth,".jpg")
-
-  for (i in 1:length(fileset.truth)) {
-    print(fileset.truth[i])
-    test.prices = readRDS(file.path(output.directory, paste0("UCD_Lehmann_", fileset.truth[i],".RDS")))$prices
-    truth.prices = readRDS(file.path(elem, paste0("UCD_Lehmann_", fileset.truth[i],"_price_truth.RDS")))$prices
+  
+  if (length(summary.output) > 0) {
+    names(summary.output) = list.files(elem, pattern = ".RDS", full.names = FALSE)
+    summary.output1 = lapply(fileset.truth, function(truth.file) {
+    
+    test.name = gsub( file.path(EVAL.INPUT.DIR, last(str_split(truth.file, "/")[[1]])),  pattern = "_price_truth", replacement = "" )
+    if ( file.exists(test.name)) {
+      test.prices = readRDS( test.name )$prices
+    } else {
+      warning("no truth file:", test.name)
+      return(NULL)
+    }
+    truth.prices =  readRDS(truth.file)$prices
+    
     compare.list = NULL
     try({compare.list = wine.compare(test.prices, truth.prices)})
+    if (!is.null(compare.list)) {
+      wine.summarize(compare.list)
+    } 
+  })
+  } else {return(NULL)}
   
-    if (!is.null(compare.list)) {summary.output[[i]] = wine.summarize(compare.list)} 
-  }
-  summary.output = do.call("rbind", summary.output)
-  summary.output$name = last(unlist(strsplit(elem, split = "/")))
-  summary.output
+  summary.output1 = do.call("rbind", summary.output1)
+  summary.output1$name = last(str_split(elem, "/")[[1]]) #name is containing folder
+  summary.output1
 })
 
 summary.output = do.call("rbind", summary.output)
 
-write.csv(summary.output, "~/Documents/DSI/wine-price-extraction/dsi/Data/summary_vs_truth.csv")
-
-# C. For files we have marks ####
-
-# See wine_truth.R
-marks_top_files_base = sapply(strsplit(marks_top_files$file.jpg, "\\."), first)
-
-file_base = marks_top_files_base[1]
-# Get the marks for the file
-tmp.marks = filter(marks_image, file == file_base)
-
-# Get prices detected
-tmp.prices = readRDS(paste0("~/Documents/DSI/wine-price-extraction/dsi/Data/marks_top_files/",file_base,".RDS"))
-
-head(tmp.marks)
-do.call("rbind", tmp.prices$page.cols$prices) %>% arrange(left) %>% head()
-
-##################################################################################################################
-# 3. Code from modifying output to create truth (if necessary, would be run before comparison making)
-##################################################################################################################
-
-
-# saveRDS(UCD_Lehmann_0011_price_truth, "~/Documents/DSI/wine-price-extraction/dsi/Data/price_id_truth/UCD_Lehmann_0011_price_truth.RDS")
-
-truth1 = "1544"
-truthdata = readRDS(paste0("~/Documents/DSI/OCR_SherryLehmann/Truth2/",truth1,"_column2.RDS"))
-try({truthdata = data.frame(truthdata)})
-truthdata = data.frame(apply(truthdata, 2, as.character), stringsAsFactors = F)
-
-#modify truth?
-truthdata$Bottle = sub(pattern = "(\\.[0-9]{1}$)",  "\\10", x = truthdata$Bottle)
-truthdata$Bottle = sub(pattern = "^0\\.",  "\\.", x = truthdata$Bottle)
-truthdata$Bottle = sub(pattern = "(^[0-9]+$)",  "\\1.00", x = truthdata$Bottle)
-truthdata$Case = sub(pattern = "(^[0-9]+$)",  "\\1.00", x = truthdata$Case)
-truthdata$Case = sub(pattern = "(\\.[0-9]{1}$)",  "\\10", x = truthdata$Case)
-truthdata$CaseArrival = sub(pattern = "(\\.[0-9]{1}$)",  "\\10", x = truthdata$CaseArrival)
-truthdata$CaseArrival = sub(pattern = "(^[0-9]+$)",  "\\1.00", x = truthdata$CaseArrival)
-
-truthdata
-
-# insert
-
-truthoutput = readRDS(paste0("~/Documents/DSI/wine-price-extraction/dsi/Data/UCD_Lehmann_",truth1,".RDS"))
-# From truthfile
-i = 2
-truthoutput$prices[[i]]$ids[[1]] = data.frame(row = 1:length(truthdata$Description),
-                                              name = as.character(truthdata$Description), stringsAsFactors = F)
-
-truthoutput$prices[[i]]$prices[[4]] = data.frame(row = 1:length(truthdata$QuartCase),
-                                                 text.new = as.character(truthdata$QuartCase),
-                                                 stringsAsFactors = F)
-
-truthoutput$prices[[i]]$prices[[2]] = data.frame(row = 1:length(truthdata$CaseArrival),
-                                                 text.new = as.character(truthdata$CaseArrival),
-                                                 stringsAsFactors = F)
-
-truthoutput$prices
-
-# Custom changes
-#1,1
-truthoutput$prices[[1]]$ids[[1]] = rbind(truthoutput$prices[[1]]$ids[[1]], c(6, "234")) %>% arrange(row)
-truthoutput$prices[[1]]$prices[[1]] = rbind(truthoutput$prices[[1]]$prices[[1]], c(8, "2.19")) %>% arrange(row)
-truthoutput$prices[[1]]$prices[[1]][1,2] = "1.49"
-#1,2
-truthoutput$prices[[1]]$prices[[2]] = rbind(truthoutput$prices[[1]]$prices[[2]], c(6, "21.50")) %>% arrange(row)
-truthoutput$prices[[1]]$prices[[2]][7,2] = "23.65"
-
-#2,1
-truthoutput$prices[[2]]$ids[[1]] = rbind(truthoutput$prices[[2]]$ids[[1]], c(0, "614")) %>% arrange(row)
-truthoutput$prices[[2]]$ids[[1]]$row = 1:8
-truthoutput$prices[[2]]$prices[[1]] = rbind(truthoutput$prices[[2]]$prices[[1]], c(0, "2.29"))
-truthoutput$prices[[2]]$prices[[1]] = truthoutput$prices[[2]]$prices[[1]] %>% arrange(row)
-truthoutput$prices[[2]]$prices[[1]]$row = 1:8
-truthoutput$prices[[2]]$prices[[1]][6,2] = "2.99"
-
-#2,2
-truthoutput$prices[[2]]$prices[[2]] = rbind(truthoutput$prices[[2]]$prices[[2]], c(0, "24.75")) %>% arrange(row)
-truthoutput$prices[[2]]$prices[[2]]$row = 1:8
-
-#names
-names(truthoutput$prices[[1]]$prices) = c("bottle", "2 bottles")
-names(truthoutput$prices[[1]]$prices) = c("Bottle","Case")
-names(truthoutput$prices[[2]]$prices) = c("Bottle","Case")
-names(truthoutput$prices[[3]]$prices) = c("Bottle")
-names(truthoutput$prices[[4]]$prices) = c("Bottle")
-names(truthoutput$prices[[1]]$prices) = c("Case NOW","Case Price Upon Arrival","You Save")
-names(truthoutput$prices[[2]]$prices) = c("Fifth Bottle","Fifth Case","Quart Bottle","Quart Case")
-
-# save
-#saveRDS(truthoutput, paste0("~/Documents/DSI/wine-price-extraction/dsi/Data/price_id_truth/UCD_Lehmann_",truth1,"_price_truth.RDS"))
+write.csv(summary.output, file.path(EVAL.OUTPUT.DIR, "summary_vs_truth.csv"))
 
