491765f0e6853236d99e78def079dfb47c1dd2d6
Jane Carlen <janecarlen@administorsMBP3.attlocal.net>
2019-05-15 22:34:22 -0700

    removed redunant textTypes function

--- dsi/R/helper.R
+++ dsi/R/helper.R
@@ -76,7 +76,6 @@ isPrice = #Jane's version - different than Duncan's isPrice
     })
   }
 
-
 numToPrice = function(numAsChar, dollar = FALSE) {
   #dollar False to not allow numbers will dollar sign in front 
   #order of options is important (don't reorder)
@@ -117,28 +116,6 @@ numToPrice = function(numAsChar, dollar = FALSE) {
   }
   
   return(FALSE)
-}
-  
-# Sort a catalog of images by instances of prices, numbers and other text
-# will inform if a page has a price table
-
-textTypes <- function(cat_files, catalog = NULL){
-  
-  if(is.null(catalog)) {catalog = sample(1:length(cat_files), 1)}
-  cat1 = cat_files[catalog][[1]]
-  cat("catalog number", catalog, "with", nrow(cat1), "files")
-  
-  text.tmp = lapply(paste(cat1$Sample, cat1$file.jpg, sep = "/"), function(x) {
-    text1 = GetText(x)
-    table1 = table(sapply(text1, isPrice, maybe = TRUE))
-    print(prop.table(table1))
-    return(data.frame(as.list(table1))) #if only one type skip -- wouldn't be a table page
-  })
-  
-  text.types= bind_rows(text.tmp)
-  names(text.types) = sapply(names(text.types), switch, "FALSE." = "other_text","number" = "number", "TRUE." = "price",
-                             "number." = "number*", "X.number" = "*number")
-  return(text.types)
 }
 
 # For precendence between detected prices
